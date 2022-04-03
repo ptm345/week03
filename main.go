4ebaf90031e273006f4786e0b24456b79711e1c9
ptm345 <549259895@qq.com>
2022-04-03 22:53:28 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -50,10 +54,29 @@ func main() {
 		}
 
 		timeoutCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		// 这里不是必须的，但是如果使用 _ 的话静态扫描工具会报错，加上也无伤大雅
 		defer cancel()
 
 		log.Println("shutting down server...")
 		return server.Shutdown(timeoutCtx)
+	})
+
+	// g3
+	// g3 捕获到 os 退出信号将会退出
+	// g3 退出了所有的协程都能退出么？
+	// g3 退出后, context 将不再阻塞，g2 会随之退出
+	// g2 退出时，调用了 shutdown，g1 会退出
+	// 然后 main 函数中的 g.Wait() 退出，所有协程都会退出
+	g.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return errors.New(fmt.Sprintf("get os signal: %v", sig))
+		}
 	})
 
 	fmt.Printf("errgroup exiting: %+v\n", g.Wait())
